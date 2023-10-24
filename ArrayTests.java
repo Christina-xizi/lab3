bfb2beb835205679737859ea6e46420980099747
Christina <146885167+Christina-xizi@users.noreply.github.com>
2023-10-24 13:20:22 -0700

    Debug

--- ArrayTests.java
+++ ArrayTests.java
@@ -11,8 +11,23 @@ public class ArrayTests {
 
 
   @Test
-  public void testReversed() {
+  public void testReversedEmpty() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseOdd() {
+    int[] input1 = {1,4,5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5, 4, 1 }, input1);
+  }
+
+
+  @Test
+  public void testReversedEven() {
+    int[] input1 = {1,4,6,8};
+    assertArrayEquals(new int[]{8,6,4,1}, ArrayExamples.reversed(input1));
+  }
+
 }
